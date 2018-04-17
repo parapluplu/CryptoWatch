138a47f97ba553c49e70df1ed05245943d2dfc0d
Felix Remmel <felix.remmel@gmail.com>
2018-04-17 23:52:12 +0200

    add method to get events by tokens

--- zeus-common/src/main/java/de/paraplu/cryptocurrency/domain/mongodb/repository/TriggerEventRepository.java
+++ zeus-common/src/main/java/de/paraplu/cryptocurrency/domain/mongodb/repository/TriggerEventRepository.java
@@ -1,6 +1,12 @@
 package de.paraplu.cryptocurrency.domain.mongodb.repository;
 
+import java.util.Collection;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import de.paraplu.cryptocurrency.domain.mongodb.pojo.trigger.TriggerEvent;
@@ -8,4 +14,6 @@ import de.paraplu.cryptocurrency.domain.mongodb.pojo.trigger.TriggerEvent;
 @Repository
 public interface TriggerEventRepository extends MongoRepository<TriggerEvent, String> {
 
+    @Query(value = "{'data.enrichedTransferMessage.tokenInfo.symbol': { '$in': ?0 } }")
+    public Page<TriggerEvent> findByTokens(@Param("tokenSymbols") Collection<String> tokenSymbol, Pageable pageable);
 }
