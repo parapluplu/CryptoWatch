1eb75f26c33b82e65b1220637591d05ed19d0827
Felix Remmel <felix.remmel@gmail.com>
2018-04-24 22:16:14 +0200

    Add dockerfile to frontend ; make service url configurable

--- aphrodite-frontend/src/app/shared/tokeninfo/tokeninfo.service.ts
+++ aphrodite-frontend/src/app/shared/tokeninfo/tokeninfo.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs/Observable';
+import { environment } from '../../../environments/environment.prod';
 
 @Injectable()
 export class TokeninfoService {
@@ -8,10 +9,10 @@ export class TokeninfoService {
   constructor(private http: HttpClient) {}
 
   getAll(): Observable<any> {
-    return this.http.get('//localhost:8001/api/tokenInfoes');
+    return this.http.get(environment.apiUrl + '/tokenInfoes');
   }
 
   getById(id: string): Observable<any> {
-    return this.http.get('//localhost:8001/api/tokenInfoes/' + id);
+    return this.http.get(environment.apiUrl +  + '/tokenInfoes/' + id);
   }
 }
--- aphrodite-frontend/src/app/shared/triggerevents/triggerevents.service.ts
+++ aphrodite-frontend/src/app/shared/triggerevents/triggerevents.service.ts
@@ -1,12 +1,13 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs/Observable';
+import { environment } from '../../../environments/environment.prod';
 
 @Injectable()
 export class TriggerEventsService {
 
   getForTokens(tokens: Array<string>, page: number, pageSize: number): any {
-    return this.http.get('//localhost:8001/api/triggerEvents/search/findByTokens', {
+    return this.http.get(environment.apiUrl + 'triggerEvents/search/findByTokens', {
       params: {
         tokenSymbols: tokens.join(','),
         page: page.toString(),
@@ -18,10 +19,10 @@ export class TriggerEventsService {
   constructor(private http: HttpClient) {}
 
   getAll(): Observable<any> {
-    return this.http.get('//localhost:8001/triggerEvents/all?sort=date,desc');
+    return this.http.get(environment.apiUrl + 'triggerEvents/all?sort=date,desc');
   }
 
   getById(id: string): Observable<any> {
-    return this.http.get('//localhost:8001/api/triggerEvents/' + id);
+    return this.http.get(environment.apiUrl + 'api/triggerEvents/' + id);
   }
 }
