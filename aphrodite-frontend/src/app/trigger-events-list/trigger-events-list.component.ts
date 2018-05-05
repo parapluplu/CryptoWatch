9367ec0d570bd6390912f646d3b0ac867b912368
Felix Remmel <felix.remmel@gmail.com>
2018-05-05 22:38:51 +0200

    rename default config file ; [frontend] add bootstrap source to project

--- aphrodite-frontend/src/app/trigger-events-list/trigger-events-list.component.ts
+++ aphrodite-frontend/src/app/trigger-events-list/trigger-events-list.component.ts
@@ -1,9 +1,9 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
-import {TriggerEventsService} from '../shared/triggerevents/triggerevents.service';
-import {TokeninfoService} from '../shared/tokeninfo/tokeninfo.service';
+import { TriggerEventsService } from '../shared/triggerevents/triggerevents.service';
+import { TokeninfoService } from '../shared/tokeninfo/tokeninfo.service';
 
-import {TriggerEventDetailsComponent} from '../trigger-event-details/trigger-event-details.component';
+import { TriggerEventDetailsComponent } from '../trigger-event-details/trigger-event-details.component';
 import { ActivatedRoute } from '@angular/router';
 import { CookieService } from 'ngx-cookie';
 
@@ -23,21 +23,21 @@ export class TriggerEventsListComponent implements OnInit {
   pages: number;
   collectionSize: number;
   updating: boolean = false;
-  
 
-  constructor(private triggerEventsService: TriggerEventsService, 
-    private tokeninfoService: TokeninfoService, 
-    private route: ActivatedRoute, 
-    private cookieService: CookieService) {}
+
+  constructor(private triggerEventsService: TriggerEventsService,
+    private tokeninfoService: TokeninfoService,
+    private route: ActivatedRoute,
+    private cookieService: CookieService) { }
 
   ngOnInit() {
     this.tokeninfoService.getAll().subscribe(data => {
       this.availableTokenInfoes = data._embedded.tokenInfoes.map(e => e.symbol);
-      if(this.cookieService.get(SELECTED_TOKENS_COOKIE) == null) {
+      if (this.cookieService.get(SELECTED_TOKENS_COOKIE) == null) {
         this.selectedTokens = this.availableTokenInfoes;
-        this.cookieService.putObject(SELECTED_TOKENS_COOKIE, this.selectedTokens);
+        this.saveCookie(this.selectedTokens);
       } else {
-        this.selectedTokens = <Array<string>> this.cookieService.getObject(SELECTED_TOKENS_COOKIE);
+        this.selectedTokens = <Array<string>>this.cookieService.getObject(SELECTED_TOKENS_COOKIE);
       }
       this.update();
     });
@@ -45,13 +45,14 @@ export class TriggerEventsListComponent implements OnInit {
 
   update() {
     this.updating = true;
-    this.triggerEventsService.getForTokens(this.selectedTokens, this.page-1, this.pageSize).subscribe(data => {
-      this.page = data.page.number+1;
+    this.saveCookie(this.selectedTokens);
+    this.triggerEventsService.getForTokens(this.selectedTokens, this.page - 1, this.pageSize).subscribe(data => {
+      this.page = data.page.number + 1;
       this.pages = data.page.totalPages;
-      this.collectionSize=data.page.totalElements;
+      this.collectionSize = data.page.totalElements;
       this.triggerEvents = data._embedded.triggerEvents;
-      if(this.page > this.pages) {
-        this.page = this.pages-1;
+      if (this.page > this.pages) {
+        this.page = this.pages - 1;
         this.update();
       } else {
         this.updating = false;
@@ -61,23 +62,30 @@ export class TriggerEventsListComponent implements OnInit {
 
   toPage(page: number): void {
     this.updating = true;
-    this.page = page+1;
+    this.page = page + 1;
     this.update();
   }
 
   change() {
     this.updating = true;
-    this.cookieService.putObject(SELECTED_TOKENS_COOKIE, this.selectedTokens);
     this.page = 1;
     this.update();
   }
-  
+
   selectAll() {
     this.selectedTokens = [];
     this.availableTokenInfoes.forEach(token => {
       this.selectedTokens.push(token);
     });
     this.change();
-  } 
+  }
+
+  saveCookie(selectedTokens: Array<string>): void {
+    const expiryDate = new Date();
+    expiryDate.setDate(expiryDate.getDate() + 30);
+    this.cookieService.putObject(SELECTED_TOKENS_COOKIE, selectedTokens, {
+      expires: expiryDate
+    });
+  }
 
 }
