157e3410303ce1fda3956878333582191eaa45d6
Felix Remmel <felix.remmel@gmail.com>
2018-05-27 23:18:03 +0200

    add exchange statistics to frontend

--- messy-aphrodite/index.js
+++ messy-aphrodite/index.js
@@ -48,11 +48,64 @@ app.get('/txnSummary', (req, res) => {
     const tokenSymbols = req.query.tokenSymbols.split(",");
     console.log(req.query);
     console.log(tokenSymbols);
+    db.collection("enrichedTransferMessage").aggregate(
+        [
+            {
+                $match: {
+                    "tokenInfo.symbol": {$in: tokenSymbols},
+                    blockDate: {$gte: new Date((new Date().getTime() - (7 * 24 * 60 * 60 * 1000)))}
+                }
+            },
+            {
+                $project: {
+                    "tokenInfo.symbol": 1,
+                    "exchange.in" : {
+                        $cond: {
+                            if: { $eq: [true, "$exchange.in"] },
+                            then: "$insecureAmount",
+                            else: "$$REMOVE"
+                        }
+                    },
+                    "exchange.out" : {
+                        $cond: {
+                            if: { $eq: [true, "$exchange.out"] },
+                            then: "$insecureAmount",
+                            else: "$$REMOVE"
+                        }
+                    }
+                }
+            },
+            {
+                $group: {
+                    _id: { symbol: "$tokenInfo.symbol" },
+                    txns: { $sum: 1 },
+                    exchange_in: {$sum: "$exchange.in"},
+                    exchange_out: {$sum: "$exchange.out"}
+                }
+            }
+        ]
+    ).toArray(function (err, result) {
+        if (err) {
+            res.statusCode = 500;
+            res.send("Unexpected exception")
+            console.error("Exception while serving request " + req + ": " + err);
+        } else {
+            res.send(result);
+        }
+    });
+});
+
+app.get('/cxSummary', (req, res) => {
+
+    const tokenSymbols = req.query.tokenSymbols.split(",");
+    console.log(req.query);
+    console.log(tokenSymbols);
     db.collection("triggerEvent").aggregate(
         [
             {
                 $match: {
                     "data.symbol": {$in: tokenSymbols},
+                    "data.exchange": true,
                     date: {$gte: new Date((new Date().getTime() - (7 * 24 * 60 * 60 * 1000)))}
                 }
             },
