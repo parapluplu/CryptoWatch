6a26abc94fb5cc7f454a8ec376a62ac53c5356bf
Felix Remmel <felix.remmel@gmail.com>
2018-05-29 22:56:28 +0200

    improve aggregation performance for exchange query

--- messy-aphrodite/index.js
+++ messy-aphrodite/index.js
@@ -17,7 +17,7 @@ if (process.env.NODE_ENV) {
 }
 configClient.load({
     name: "application",
-    endpoint : localConfig.get("config-server-uri"),
+    endpoint: localConfig.get("config-server-uri"),
     profiles: profile
 }).then((config) => {
     const mongoUri = config.get("spring.data.mongodb.uri");
@@ -45,6 +45,33 @@ configClient.load({
 
 app.get('/txnSummary', (req, res) => {
 
+    function exchangeInfo(tokenSymbols, field, cb) {
+        db.collection("enrichedTransferMessage").aggregate(
+            [
+                {
+                    $match: {
+                        "tokenInfo.symbol": { $in: tokenSymbols },
+                        blockDate: { $gte: new Date((new Date().getTime() - (7 * 24 * 60 * 60 * 1000))) },
+                        [field]: true
+                    }
+                },
+                {
+                    $group: {
+                        _id: { symbol: "$tokenInfo.symbol" },
+                        exchange_sum: { $sum: "$insecureAmount" }
+                    }
+                }
+            ]
+        ).toArray(function (err, result) {
+            if (err) {
+                console.error("Exception while serving request " + req + ": " + err);
+                cb(err, null);
+            } else {
+                cb(null, result);
+            }
+        });
+    }
+
     const tokenSymbols = req.query.tokenSymbols.split(",");
     console.log(req.query);
     console.log(tokenSymbols);
@@ -52,45 +79,44 @@ app.get('/txnSummary', (req, res) => {
         [
             {
                 $match: {
-                    "tokenInfo.symbol": {$in: tokenSymbols},
-                    blockDate: {$gte: new Date((new Date().getTime() - (7 * 24 * 60 * 60 * 1000)))}
-                }
-            },
-            {
-                $project: {
-                    "tokenInfo.symbol": 1,
-                    "exchange.in" : {
-                        $cond: {
-                            if: { $eq: [true, "$exchange.in"] },
-                            then: "$insecureAmount",
-                            else: "$$REMOVE"
-                        }
-                    },
-                    "exchange.out" : {
-                        $cond: {
-                            if: { $eq: [true, "$exchange.out"] },
-                            then: "$insecureAmount",
-                            else: "$$REMOVE"
-                        }
-                    }
+                    "tokenInfo.symbol": { $in: tokenSymbols },
+                    blockDate: { $gte: new Date((new Date().getTime() - (7 * 24 * 60 * 60 * 1000))) }
                 }
             },
             {
                 $group: {
                     _id: { symbol: "$tokenInfo.symbol" },
-                    txns: { $sum: 1 },
-                    exchange_in: {$sum: "$exchange.in"},
-                    exchange_out: {$sum: "$exchange.out"}
+                    txns: { $sum: 1 }
                 }
             }
         ]
-    ).toArray(function (err, result) {
+    ).toArray(function (err, txInfo) {
         if (err) {
             res.statusCode = 500;
             res.send("Unexpected exception")
             console.error("Exception while serving request " + req + ": " + err);
         } else {
-            res.send(result);
+            exchangeInfo(tokenSymbols, "exchange.in", function (err, xInfoIn) {
+                if (err) {
+                    res.statusCode = 500;
+                    res.send("Unexpected exception")
+                    console.error("Exception while serving request " + req + ": " + err);
+                } else {
+                    exchangeInfo(tokenSymbols, "exchange.out", function (err, xInfoOut) {
+                        var result = []
+                        console.log(xInfoOut)
+                        console.log(xInfoIn)
+                        txInfo.forEach(row => {
+                            console.log(row);
+                            const symbol = row["_id"]["symbol"];
+                            result.push(row);
+                            merge(xInfoIn, symbol, row, "exchange_in");
+                            merge(xInfoOut, symbol, row, "exchange_out");
+                        });
+                        res.send(result);
+                    });
+                }
+            });
         }
     });
 });
@@ -104,9 +130,9 @@ app.get('/cxSummary', (req, res) => {
         [
             {
                 $match: {
-                    "data.symbol": {$in: tokenSymbols},
+                    "data.symbol": { $in: tokenSymbols },
                     "data.exchange": true,
-                    date: {$gte: new Date((new Date().getTime() - (7 * 24 * 60 * 60 * 1000)))}
+                    date: { $gte: new Date((new Date().getTime() - (7 * 24 * 60 * 60 * 1000))) }
                 }
             },
             {
@@ -126,4 +152,15 @@ app.get('/cxSummary', (req, res) => {
         }
     });
 });
+
+function merge(xInfo, symbol, row, field) {
+    var val = 0;
+    for(let xi of xInfo) {
+        if (xi["_id"]["symbol"] == symbol) {
+            val = xi["exchange_sum"];
+            break;
+        }
+    }
+    row[field] = val;
+}
 
